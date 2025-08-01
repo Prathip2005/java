4c63a8e27e8245006bd0d1febc3c89f2b0fbb9e5
Prathip2005 <prathipsiva2005@gmail.com>
2025-08-01 19:08:24 +0530

    operators

---
+++ chapter1/operators.java
@@ -0,0 +1,13 @@
+public class operators {
+   public static void main(String[] args)
+{
+     int a=10;
+     int b=20;
+     int c=a+b;
+     int d=a*b;
+     int e=a/b;
+        System.out.println("(a + b): " + c);
+        System.out.println("(a * b): " + d);
+        System.out.println("(a / b): " + e);
+  }
+}
